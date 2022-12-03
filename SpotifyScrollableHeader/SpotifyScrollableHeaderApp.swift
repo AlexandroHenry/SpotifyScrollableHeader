fae32006a224d78a14fa72e7bbbd9dd65d13037d
alaxhenry <alaxhenry0121@gmail.com>
2022-12-03 12:22:38 +0900

    Initial Commit

---
+++ SpotifyScrollableHeader/SpotifyScrollableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyScrollableHeaderApp.swift
+//  SpotifyScrollableHeader
+//
+//  Created by Seungchul Ha on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyScrollableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
